e720ee947280acaf883ea587e3c7d51ac5271bc6
Dirk Jan Wassink <dirk.jan.wassink@gmail.com>
2024-02-16 11:33:10 +0100

    Upgraded all dev dependencies

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,30 +1,21 @@
-import commonjs from 'rollup-plugin-commonjs';
-import sourceMaps from 'rollup-plugin-sourcemaps';
-import {terser} from 'rollup-plugin-terser';
-import typescript from 'rollup-plugin-typescript2';
-
-const pkg = require('./package.json');
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
 
 const sharedConfig = {
     input: `src/throttle.ts`,
     watch: {
-        include: 'src/**'
+        include: 'src/**',
     },
     plugins: [
         // Compile TypeScript files
-        typescript({useTsconfigDeclarationDir: true}),
-        // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-        commonjs(),
-        // Resolve source maps to the original source
-        sourceMaps(),
+        typescript(),
         // Minify the output
-        terser()
-    ]
+        terser(),
+    ],
 };
-
 const outputs = [
-    {file: pkg.main, name: 'throttle', format: 'umd', sourcemap: true},
-    {file: pkg.module, name: 'throttle', format: 'es', sourcemap: true}
+    {file: 'dist/lib/throttle.js', name: 'throttle', format: 'umd', sourcemap: true},
+    {file: 'dist/lib/throttle.mjs', name: 'throttle', format: 'es', sourcemap: true},
 ];
 
-export default outputs.map(output => ({...sharedConfig, output}));
+export default outputs.map((output) => ({...sharedConfig, output}));
